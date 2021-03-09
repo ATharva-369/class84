bc2202d979775121d7a52ef62e1afdcb868373a9
ATharva-369 <gameboy369065@gmail.com>
2021-03-10 00:59:44 +0400

    Add files via upload

---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { AppTabNavigator } from './AppTabNavigator'
+import CustomSideBarMenu  from './CustomSideBarMenu';
+import MyDonationScreen from '../screens/MyDonationScreen';
+import SettingScreen from '../screens/SettingScreen';
+import Notification from '../screens/NotificationScreen'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+  Home : {
+    screen : AppTabNavigator
+    },
+  MyDonations : {
+    screen : MyDonationScreen
+  },
+  Notification:{
+    screen:Notification
+  }
+  ,
+  Setting : {
+    screen : SettingScreen
+  }
+},
+  {
+    contentComponent:CustomSideBarMenu
+  },
+  {
+    initialRouteName : 'Home'
+  })
